a50114408f4d76b9abf48dbcaacd174730d164e0
martbakCode <rajendranohan4@gmail.com>
2023-10-23 09:08:14 +0700

    About Us Update

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -2,13 +2,13 @@
 import streamlit as st
 
 st.markdown("# Sistem Informasi Prediksi Harga IHSG berbasis Machine Learning")
-st.caption("Sistem Informasi ini dapat digunakan untuk membandingkan prediksi harga IHSG menggunakan beberapa algoritma machine learning, yaitu kNN, Regresi Linear,Random Forest dan Decision Tree")
-st.caption("Pengembangan sistem informasi ini didukung oleh penelitian dengan pendanaan dari Hibah Internal Semester Genap 2022-2023 di Universitas Dian Nuswantoro.")
+st.subheader("Sistem Informasi ini dapat digunakan untuk membandingkan prediksi harga IHSG menggunakan beberapa algoritma machine learning, yaitu kNN, Regresi Linear,Random Forest dan Decision Tree")
+st.subheader("Pengembangan sistem informasi ini didukung oleh penelitian dengan pendanaan dari Hibah Internal Semester Genap 2022-2023 di Universitas Dian Nuswantoro.")
 
-st.text('Tim Peneliti:')
-st.text('1. Dr. Ir. Dwi Eko Waluyo, MM (NIDN: 0630106502)')
-st.text('2. Hayu Wikan Kinasih, M.Si (NIDN: 0625118701)')
-st.text('3. Dewi Pergiwati, M.Kom (NIDN: 0608029401)')
-st.text('4. Cinantya Paramita, M.Eng (NIDN: 0613128902)')
-st.text('5. Fauzi Adi Rafrastara, M.CS (NIDN: 0630048802)') 
-st.text('6. Rajendra Nohan (NIM: A11.2021.13301)')
+st.caption('Tim Peneliti:')
+st.caption('1. Dr. Ir. Dwi Eko Waluyo, MM (NIDN: 0630106502)')
+st.caption('2. Hayu Wikan Kinasih, M.Si (NIDN: 0625118701)')
+st.caption('3. Dewi Pergiwati, M.Kom (NIDN: 0608029401)')
+st.caption('4. Cinantya Paramita, M.Eng (NIDN: 0613128902)')
+st.caption('5. Fauzi Adi Rafrastara, M.CS (NIDN: 0630048802)') 
+st.caption('6. Rajendra Nohan (NIM: A11.2021.13301)')
