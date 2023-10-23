9887bc9b35077b8af352ed4e47093d44ef189c8d
martbakCode <rajendranohan4@gmail.com>
2023-10-23 09:05:33 +0700

    About Us Update

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -1,5 +1,14 @@
 # Contents of ~/my_app/pages/page_2.py
 import streamlit as st
 
-st.markdown("# About Us")
-st.sidebar.markdown("# About Us")
+st.markdown("# Sistem Informasi Prediksi Harga IHSG berbasis Machine Learning")
+st.text("Sistem Informasi ini dapat digunakan untuk membandingkan prediksi harga IHSG menggunakan beberapa algoritma machine learning, yaitu kNN, Regresi Linear,Random Forest dan Decision Tree")
+st.text("Pengembangan sistem informasi ini didukung oleh penelitian dengan pendanaan dari Hibah Internal Semester Genap 2022-2023 di Universitas Dian Nuswantoro.")
+
+st.text('Tim Peneliti:')
+st.text('1. Dr. Ir. Dwi Eko Waluyo, MM (NIDN: 0630106502)')
+st.text('2. Hayu Wikan Kinasih, M.Si (NIDN: 0625118701)')
+st.text('3. Dewi Pergiwati, M.Kom (NIDN: 0608029401)')
+st.text('4. Cinantya Paramita, M.Eng (NIDN: 0613128902)')
+st.text('5. Fauzi Adi Rafrastara, M.CS (NIDN: 0630048802)') 
+st.text('6. Rajendra Nohan (NIM: A11.2021.13301)')
